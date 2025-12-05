6acbb7e7b62160d7d47c23c588bb9a49b0228a7f
Franco Catas <franco.catas@hotmail.com>
2025-12-05 17:39:11 -0300

    Fix for clooudflare deploy

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -2,6 +2,8 @@ import { getRequestConfig } from "next-intl/server";
 import { hasLocale } from "next-intl";
 import { routing } from "./routing";
 
+export const runtime = "edge";
+
 export default getRequestConfig(async ({ requestLocale }) => {
   const requested = await requestLocale;
   const locale = hasLocale(routing.locales, requested)
